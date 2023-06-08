051367b83fed4d8094c98bad7ea2f9b1f44407f0
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-08 23:15:33 +0700

    feat: update molecule component navbar

--- src/components/molecules/Navbar/index.jsx
+++ src/components/molecules/Navbar/index.jsx
@@ -1,36 +1,30 @@
 import { Button } from "@components/atoms";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const tokenExist = localStorage.getItem("key");
+
   return (
     <nav>
       <div className="w-full flex flex-wrap items-center justify-between py-3 px-4 fixed bg-[rgb(0,0,0)]/70 z-10">
-        <a href="#" className="flex items-center text-white font-bold text-4xl">
-          PEOPL.
-        </a>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+        <div className="flex items-center justify-center gap-4">
+          <a
+            href="#"
+            className="flex items-center text-white font-bold text-4xl"
           >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <Button />
+            PEOPL.
+          </a>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/movies"}>Movies</Link>
+        </div>
+        <div className="block w-auto" id="navbar-default">
+          {tokenExist ? (
+            <>
+              <div>Welcome</div>
+            </>
+          ) : (
+            <Button />
+          )}
         </div>
       </div>
     </nav>
