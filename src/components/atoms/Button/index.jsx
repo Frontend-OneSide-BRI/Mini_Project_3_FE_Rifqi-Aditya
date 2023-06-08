91f10a69f38c939cfcf484213b19e5943d242445
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-08 18:19:26 +0700

    style: update button styling

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -1,6 +1,6 @@
 const Button = ({ btnText = "SIGN UP NOW" }) => {
   return (
-    <div className="bg-blue-primary rounded-lg">
+    <div className="bg-stone-700 rounded-lg">
       <p className="text-white font-semibold px-6 py-3 text-sm">{btnText}</p>
     </div>
   );
