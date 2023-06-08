e7b32cfd7db57f91a16516cf6a36024e0a279903
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-08 15:06:03 +0700

    feat: setup constants

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+const API_BASE_URL = import.meta.env.VITE_BASE_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+const API_AUTH_TOKEN = import.meta.env.VITE_API_AUTH_TOKEN;
+
+const TMDB_IMG_BASE_URL = "https://image.tmdb.org/t/p/original";
+
+export { API_BASE_URL, API_KEY, API_AUTH_TOKEN, TMDB_IMG_BASE_URL };
