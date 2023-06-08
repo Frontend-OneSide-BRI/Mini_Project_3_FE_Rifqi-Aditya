ea5f50d75749e1b1f10c3b62fc699c8b6870fdd3
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-08 15:05:11 +0700

    feat: create cnavbar molecules component

---
+++ src/components/molecules/Navbar/index.jsx
@@ -0,0 +1,40 @@
+import { Button } from "@components/atoms";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="w-full flex flex-wrap items-center justify-between py-3 px-4 fixed bg-[rgb(0,0,0)]/70">
+        <a href="#" className="flex items-center text-white font-bold text-4xl">
+          PEOPL.
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <Button />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
