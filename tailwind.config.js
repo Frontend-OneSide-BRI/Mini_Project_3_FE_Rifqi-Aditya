de86ea53268e5bc54280bd8e2344053c21efc086
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-08 23:17:46 +0700

    chore: update package and scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "blue-primary": "#0021b2",
+      },
+    },
   },
   plugins: [],
 };
