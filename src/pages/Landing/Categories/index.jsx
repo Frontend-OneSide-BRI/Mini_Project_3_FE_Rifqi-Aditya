e26e463a373eda69917e7750763a112b87cdf0a4
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-08 15:06:30 +0700

    feat: create Landing page specific component ComingSoon

---
+++ src/pages/Landing/Categories/index.jsx
@@ -0,0 +1,24 @@
+import { useGetPopularMoviesQuery } from "@redux/services/movies/moviesApi";
+import { sliceArray } from "@utils";
+import { TMDB_IMG_BASE_URL } from "@constants";
+import { Card } from "@components/atoms";
+
+const Categories = ({ data }) => {
+  return (
+    <section>
+      <div className="flex flex-col items-center test">
+        <div className="pb-28">
+          <div className="mt-40 mb-5">
+            <h3 className="mb-2 text-2xl font-bold tracking-widest">
+              THE ONE TO WATCH
+            </h3>
+            <h2 className="text-6xl font-extralight">For the Biggest Hits</h2>
+          </div>
+          <Card results={data.results} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Categories;
