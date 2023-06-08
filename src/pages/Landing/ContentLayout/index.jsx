617b2ff1a6d01199ff872197740a893d1cade7ca
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-08 18:20:52 +0700

    style: update content layout styling

--- src/pages/Landing/ContentLayout/index.jsx
+++ src/pages/Landing/ContentLayout/index.jsx
@@ -1,5 +1,7 @@
 const ContentLayout = ({ children }) => {
-  return <div className="py-28 px-44">{children}</div>;
+  return (
+    <div className="p-14 sm:p-20 md:p-28 lg:py-28 lg:px-44">{children}</div>
+  );
 };
 
 export default ContentLayout;
