10ac246fef2867883698ed49f4f054ff055faa1d
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-08 18:21:51 +0700

    refactor: update imports

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -2,3 +2,4 @@ export { default as Button } from "./Button";
 export { default as Card } from "./Card";
 export { default as Spinner } from "./Spinner";
 export { default as SkeletonLoader } from "./SkeletonLoader";
+export { default as SearchBar } from "./SearchBar";
