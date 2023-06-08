4774e9c162ee2770013bff970f80aca5e89f119b
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-08 18:20:05 +0700

    feat: update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "@routes/routes";
 import { Provider } from "react-redux";
 import { store } from "@redux/store.js";
+import { Routes, Route } from "react-router-dom";
+import Landing from "@pages/Landing";
+import Movies from "@pages/Movie";
 
 function App() {
   return (
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/movie/:movieId" element={<Movies />} />
+      </Routes>
     </Provider>
   );
 }
