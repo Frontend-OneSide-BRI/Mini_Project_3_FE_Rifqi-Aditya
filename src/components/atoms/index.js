aa734b2412a9d335cc0f8dbd2a0437a07bfaba38
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-08 23:14:57 +0700

    feat: update imports

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,5 +1,4 @@
 export { default as Button } from "./Button";
 export { default as Card } from "./Card";
-export { default as Spinner } from "./Spinner";
-export { default as SkeletonLoader } from "./SkeletonLoader";
 export { default as SearchBar } from "./SearchBar";
+export { default as Pagination } from "./Pagination";
