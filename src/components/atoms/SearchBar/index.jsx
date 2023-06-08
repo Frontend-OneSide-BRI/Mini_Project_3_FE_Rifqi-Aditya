d159e62175f70147d4b67aa78095ca4677ff3848
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-08 23:14:35 +0700

    feat: update atom components search bar

--- src/components/atoms/SearchBar/index.jsx
+++ src/components/atoms/SearchBar/index.jsx
@@ -9,9 +9,7 @@ const SearchBar = () => {
 
   return (
     <form className="flex items-center">
-      <label for="simple-search" className="sr-only">
-        Search
-      </label>
+      <label className="sr-only">Search</label>
       <div className="relative w-full mx-5 sm:mx-0">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <svg
@@ -22,9 +20,9 @@ const SearchBar = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         </div>
