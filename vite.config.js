39e4221bcc975e73bb7e280e1bdbe063652738d4
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-08 15:00:43 +0700

    chore: add config for absolute paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
+      "@redux": path.resolve(__dirname, "./src/redux"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
+      "@services": path.resolve(__dirname, "./src/redux/services"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+    },
+  },
+});
