b5c9c3884cc68305025ceb7e94cd48eb765b31a5
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-08 23:15:48 +0700

    feat: update organism component content layout

---
+++ src/components/organism/ContentLayout/index.jsx
@@ -0,0 +1,9 @@
+const ContentLayout = ({ children }) => {
+  return (
+    <div className="p-10 sm:p-20 md:p-28 lg:py-28 lg:pt-28 lg:pb-20 lg:px-44">
+      {children}
+    </div>
+  );
+};
+
+export default ContentLayout;
